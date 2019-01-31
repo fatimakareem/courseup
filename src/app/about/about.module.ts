50431d7aad097fddca6cec7eded55191a27d4f15
unknown <you@example.com>
2019-01-31 14:25:51 +0500

    ssss

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,32 @@
+import {Routes, RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {AboutComponent} from "./about.component";
+import {BenefitsModule} from "../benefits/benefits.module";
+
+
+const aboutRoutes: Routes = [
+  {path: '', component: AboutComponent},
+];
+
+
+@NgModule({
+  declarations: [
+    AboutComponent
+  ],
+
+  imports: [
+    CommonModule,
+    RouterModule.forChild(aboutRoutes),
+    BenefitsModule
+  ],
+
+  providers: [],
+  exports: []
+
+})
+
+export class AboutModule {
+
+}
+
